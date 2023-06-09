989d1e9326f8ea04ec022918fc778a2c6e0427d6
franbotteroprogramacion <123417756+franbotteroprogramacion@users.noreply.github.com>
2023-06-09 12:31:37 -0300

    Add files via upload

---
+++ Pagina Web/HTML/function.js
@@ -0,0 +1,23 @@
+function inicio(){
+    location.href = "http://127.0.0.1:5500/index.html"
+}
+
+function Budines(){
+    location.href = "http://127.0.0.1:5500/pagina/Budines.html"
+}
+
+function Delicatessen(){
+    location.href = "http://127.0.0.1:5500/pagina/Delicatessen.html"
+}
+
+function Especialidades(){
+    location.href = "http://127.0.0.1:5500/pagina/Especialidades.html"
+}
+
+function Tartas(){
+    location.href = "http://127.0.0.1:5500/pagina/Tartas.html"
+}
+
+function Tortas(){
+    location.href = "http://127.0.0.1:5500/pagina/Tortas.html"
+}
